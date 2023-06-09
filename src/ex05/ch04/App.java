69b3170c1696c81b7b71d0e38390f932e553c897
Merhong <zx2938@gmail.com>
2023-06-09 12:06:25 +0900

    필드 private화 & get/set 사용

--- src/ex05/ch04/App.java
+++ src/ex05/ch04/App.java
@@ -3,9 +3,13 @@ package ex05.ch04;
 public class App {
 
     static void attack(ProtossUnit u1, ProtossUnit u2) {
-        u2.hp = u2.hp - u1.attack;
-        System.out.println(u2.name + "이 공격당했습니다");
-        System.out.println(u2.name + "의 남은 hp : " + u2.hp);
+        u2.setHp(u2.getHp() - u1.getAttack());
+        System.out.println(u2.getName() + "이 공격 당했습니다.");
+        System.out.println(u2.getName() + "의 남은 hp :" + u2.getHp());
+
+        // u2.hp = u2.hp - u1.attack;
+        // System.out.println(u2.name + "이 공격당했습니다");
+        // System.out.println(u2.name + "의 남은 hp : " + u2.hp);
     }
 
     public static void main(String[] args) {
--- src/ex05/ch04/ProtossUnit.java
+++ src/ex05/ch04/ProtossUnit.java
@@ -2,13 +2,37 @@ package ex05.ch04;
 
 // 추상클래스
 public abstract class ProtossUnit {
-    String name;
-    int hp;
-    int attack;
+    private String name;
+    private int hp;
+    private int attack;
     
     public ProtossUnit(String name, int hp, int attack) {
         this.name = name;
         this.hp = hp;
+        this.attack = attack;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
+
+    public int getAttack() {
+        return attack;
+    }
+
+    public void setAttack(int attack) {
         this.attack = attack;
     }
 }
