bc0060f2fbd2c00844b010d562d456b1c797c03b
Merhong <zx2938@gmail.com>
2023-06-20 13:34:48 +0900

    컬렉션 - 벡터 문제점 제거

--- src/ex13/VectorTest.java
+++ src/ex13/VectorTest.java
@@ -4,17 +4,20 @@ import java.util.Vector;
 
 public class VectorTest {
     public static void main(String[] args) {
-        Vector vc = new Vector();
+        // Vector vc = new Vector();
+        Vector<String> vc = new Vector<>();
 
         vc.add("Hello World!");
-        vc.add(new Integer(10));
-        vc.add(20);
+        vc.add("10");
+        vc.add("20");
+        // vc.add(new Integer(10));
+        // vc.add(20);
 
         System.out.println("Vector Size :" +vc.size());
 
         for(int i=0; i < vc.size(); i++) {
             System.out.println("Vector element "+ i + ": "+ vc.get(i));
         }
-        String s = (String) vc.get(0);
+        // String s = (String) vc.get(0);
     }
 }
