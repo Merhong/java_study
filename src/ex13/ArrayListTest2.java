1d4aa48636566a9fefd78749697af3eef342834b
Merhong <zx2938@gmail.com>
2023-06-20 12:22:02 +0900

    Collection - ArrayList

---
+++ src/ex13/ArrayListTest2.java
@@ -0,0 +1,18 @@
+package ex13;
+
+import java.util.ArrayList;
+
+public class ArrayListTest2 {
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<>();
+        list.add("Apple");
+        list.add("Banana");
+        list.add("Mango");
+        list.add("Pear");
+        list.add("Grape");
+
+        int index = list.indexOf("Mango");
+
+        System.out.println("Mango의 위치:"+index);
+    }
+}
