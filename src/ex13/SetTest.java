0cff65dc9352a71eb5e2f7a714525e86b1f36249
Merhong <zx2938@gmail.com>
2023-06-20 12:34:43 +0900

    컬렉션 - Set

---
+++ src/ex13/SetTest.java
@@ -0,0 +1,24 @@
+package ex13;
+
+import java.util.*;
+
+public class SetTest {
+    public static void main(String[] args) {
+        HashSet<String> set = new HashSet<>();               // 순서 없음
+        // LinkedHashSet<String> set = new LinkedHashSet<>();   // 삽입(입력)된 순서대로
+        // TreeSet<String> set = new TreeSet<>();                  // 값에 따라 순서 결정, 여기선 알파벳
+
+        set.add("Milk");
+        set.add("Bread");
+        set.add("Butter");
+        set.add("Cheese");
+        set.add("Ham");
+        set.add("Ham");
+
+        System.out.println(set);
+
+        if(set.contains("Ham")){
+            System.out.println(("Ham도 포함되어 있음"));
+        }
+    }
+}
